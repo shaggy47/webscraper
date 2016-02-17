8c737120f42d5515c34cd19aa190e9aef57aac31
vaibhavf <vaibhavpfatkal@gmail.com>
2016-02-17 20:29:28 +0530

    Initial Commit

---
+++ FineScraper/Core/Interfaces/IInputHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FineScraper.Core.Interfaces
+{
+    interface IInputHandler : IDisposable
+    {
+        /// <summary>
+        /// Property for accessing contents of a loaded resource.
+        /// </summary>
+        string source { get; }
+
+        /// <summary>
+        /// Method loads given resource into memory
+        /// </summary>
+        /// <param name="uri">Path of a resource to be loaded. It can also signify file path, or a url of a network resource.</param>
+        /// <returns></returns>
+        bool Load(string uri);
+
+
+    }
+}
---
+++ FineScraper/ReadMe.txt
@@ -0,0 +1,26 @@
+﻿Text scraper
+Regex Scraper
+WebCrawler Level wise crawling
+Image Scraper
+
+-- Input Handler
+	-- Functions as a url grabber
+	-- Loads a web page into memory
+	-- Returns back Page source
+
+-- Element Handler
+	-- Gathers information about a web element.
+	-- Collects Text/Image from page source.
+-- Extractor
+	-- Accepts Page source as an input
+	-- Extracts Information based on categories.
+	
+-- Result Generator
+	-- Accepts collected raw information from Extractors
+	-- Exports the processed information in various formats.
+	
+-- Content Formatters
+	-- Text formatter
+	-- List formatter
+	-- Table Formatter
+
